203ee86ebbf0aa2c25a1cccae769b30eba39ffc5
Luis Casas <luisscasas45@gmail.com>
2024-09-24 17:36:35 -0600

    FEAT/ Clean Architecture // Verify server is running on correct port

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "ManteniPro APIv1"
+    });
+});
+
+module.exports = app;
