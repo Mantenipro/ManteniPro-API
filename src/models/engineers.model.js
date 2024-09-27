0f0c9cfb71fc32553d9c64fe22e965370953f908
AleCalz <calzadaloyaalejandra@gmail.com>
2024-09-26 23:31:56 -0600

    Add subscription, email, and password to company; email and password to engineer

--- src/models/engineers.model.js
+++ src/models/engineers.model.js
@@ -1,29 +1,30 @@
 const mongoose = require('mongoose')
 
-const nameModel = 'Company'
 const schema = new mongoose.Schema({
+
   name: {
     type: String,
     require: true,
     maxLength: 50,
     minLength: 3
   },
-  phone_number: {
-    type: Number,
-    require: true,
-    maxLength: 10,
-    minLength: 10
-  },
-  address: {
+  email: {
     type: String,
-    require: true,
-    maxLength: 250,
-    minLength: 10
+    required: true,
+    match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
+  },
+  password: {
+    type: String,
+    require: true
   },
   status: {
     type: String,
     enum: ['active', 'inactive'],
     require: true
+  },
+  company: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'companies'
   },
   created_at: {
     type: Date,
@@ -37,4 +38,4 @@ const schema = new mongoose.Schema({
   }
 })
 
-module.exports = mongoose.model(nameModel, schema)
+module.exports = mongoose.model('engineers', schema)
