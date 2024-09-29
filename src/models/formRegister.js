f755852d25ad9127d620af78e4e778e00d58cf1c
Joseph Colin <joelbenjosephc@gmail.com>
2024-09-28 20:59:29 -0600

    Se agrega logica para que cuando se registre un usuario se copie a el schema de user perfil

--- src/models/formRegister.js
+++ src/models/formRegister.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 const mongoose = require('mongoose')
+const userPerfil = require('./user.created.perfil') // Importa el modelo userPerfil
 
 const modelName = 'Register'
 
@@ -27,12 +28,25 @@ const registerSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  usuarioId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'UserPerfil',
-    required: true
-  },
-  fechaRegistro: {type: Date, default: Date.now}
+  fechaRegistro: { type: Date, default: Date.now }
+})
+
+// Middleware para crear un registro en user.created.perfil después de guardar un documento en Register
+registerSchema.post('save', async function (doc) {
+  try {
+    const perfilData = {
+      name: doc.fullname,
+      lastname: 'defaultLastname', // Ajusta según tus necesidades
+      email: doc.email,
+      password: doc.password,
+      role: 'defaultRole', // Ajusta según tus necesidades
+      type: 'defaultType', // Ajusta según tus necesidades
+      photo: 'defaultPhoto' // Ajusta según tus necesidades
+    }
+    await userPerfil.create(perfilData)
+  } catch (err) {
+    console.error('Error creating userPerfil document', err)
+  }
 })
 
 const model = mongoose.model(modelName, registerSchema)
