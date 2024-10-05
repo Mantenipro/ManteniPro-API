36804cc0a4081d8b6f995754f50901b70b8a951e
Luis Casas <luisscasas45@gmail.com>
2024-10-04 19:21:47 -0600

    FEAT/ Add CRUD endpoints: delete, get all, get by ID, update

--- src/routes/report.router.js
+++ src/routes/report.router.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
 const express = require('express');
 const createError = require('http-errors');
 const reportUseCase = require('../usecases/report.usecase');
-const Report = require('../models/report.model'); 
+const Report = require('../models/report.model');
 
 const router = express.Router();
 
@@ -33,6 +31,96 @@ router.post('/', async (req, res) => {
         res.status(500).json({
             success: false,
             error: 'Error al crear el reporte',
+        });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const reports = await Report.find();
+        res.status(200).json({
+            success: true,
+            data: reports,
+        });
+    } catch (error) {
+        console.error('Error al obtener los reportes:', error);
+        res.status(500).json({
+            success: false,
+            error: 'Error al obtener los reportes',
+        });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const report = await Report.findById(req.params.id);
+        if (!report) {
+            return res.status(404).json({
+                success: false,
+                error: 'Reporte no encontrado',
+            });
+        }
+        res.status(200).json({
+            success: true,
+            data: report,
+        });
+    } catch (error) {
+        console.error('Error al obtener el reporte:', error);
+        res.status(500).json({
+            success: false,
+            error: 'Error al obtener el reporte',
+        });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { title, image, description, user, company } = req.body;
+        const updatedReport = await Report.findByIdAndUpdate(
+            req.params.id,
+            { title, image, description, user, company },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedReport) {
+            return res.status(404).json({
+                success: false,
+                error: 'Reporte no encontrado',
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: updatedReport,
+        });
+    } catch (error) {
+        console.error('Error al modificar el reporte:', error);
+        res.status(500).json({
+            success: false,
+            error: 'Error al modificar el reporte',
+        });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedReport = await Report.findByIdAndDelete(req.params.id);
+        if (!deletedReport) {
+            return res.status(404).json({
+                success: false,
+                error: 'Reporte no encontrado',
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: deletedReport,
+        });
+    } catch (error) {
+        console.error('Error al eliminar el reporte:', error);
+        res.status(500).json({
+            success: false,
+            error: 'Error al eliminar el reporte',
         });
     }
 });
