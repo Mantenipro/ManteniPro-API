9a67e802d0d9318c28fc5a5e1ca37859474ab0a3
Luis Casas <luisscasas45@gmail.com>
2024-10-05 11:19:21 -0600

    FEAT/ Add CRUD endpoints (Delete, Edit, GetById, Get) for ticket-report

--- src/usecases/report.usecase.js
+++ src/usecases/report.usecase.js
@@ -1,11 +1,8 @@
-/* eslint-disable no-undef */
 const createError = require('http-errors');
 const Report = require('../models/report.model');
 
-
 async function createReport(title, image, description, user, company) {
     try {
-        
         if (!title || !description || !user || !company) {
             throw createError(400, 'Faltan campos requeridos');
         }
@@ -24,6 +21,60 @@ async function createReport(title, image, description, user, company) {
     }
 }
 
+async function getAllReports() {
+    try {
+        const reports = await Report.find();
+        return reports;
+    } catch (error) {
+        throw createError(500, `Error al obtener los reportes: ${error.message}`);
+    }
+}
+
+async function getReportById(id) {
+    try {
+        const report = await Report.findById(id);
+        if (!report) {
+            throw createError(404, 'Reporte no encontrado');
+        }
+        return report;
+    } catch (error) {
+        throw createError(500, `Error al obtener el reporte: ${error.message}`);
+    }
+}
+
+async function updateReport(id, title, image, description, user, company) {
+    try {
+        const updatedReport = await Report.findByIdAndUpdate(
+            id,
+            { title, image, description, user, company },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedReport) {
+            throw createError(404, 'Reporte no encontrado');
+        }
+        return updatedReport;
+    } catch (error) {
+        throw createError(500, `Error al modificar el reporte: ${error.message}`);
+    }
+}
+
+async function deleteReport(id) {
+    try {
+        const deletedReport = await Report.findByIdAndDelete(id);
+        if (!deletedReport) {
+            throw createError(404, 'Reporte no encontrado');
+        }
+        return deletedReport;
+    } catch (error) {
+        throw createError(500, `Error al eliminar el reporte: ${error.message}`);
+    }
+}
+
 module.exports = {
     createReport,
+    getAllReports,
+    getReportById,
+    updateReport,
+    deleteReport,
 };
