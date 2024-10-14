6dd8b705418b36cd1fdac871a3d187eb8b7cfe2b
Joseph Colin <joelbenjosephc@gmail.com>
2024-10-14 09:36:39 -0600

    Se modifica el archivo src/lib/stripeService.js y src/routes/paymentRoutes.js

--- src/lib/stripeService.js
+++ src/lib/stripeService.js
@@ -3,11 +3,18 @@ const stripe = require('../config/stripeConfig')
 
 async function getProductsAndPrices() {
   try {
+    // Obtener productos y precios de Stripe
     const products = await stripe.products.list()
     const prices = await stripe.prices.list()
 
+    // Organizar los precios de menor a mayor (unit_amount representa el precio en centavos)
+    const sortedPrices = prices.data.sort(
+      (a, b) => a.unit_amount - b.unit_amount
+    )
+
+    // Mapear los productos con sus respectivos precios ordenados
     const productDetails = products.data.map((product) => {
-      const productPrices = prices.data.filter(
+      const productPrices = sortedPrices.filter(
         (price) => price.product === product.id
       )
       return {
@@ -16,7 +23,17 @@ async function getProductsAndPrices() {
       }
     })
 
-    return productDetails
+    // Ordenar los productos por el precio más bajo de cada producto
+    const sortedProductDetails = productDetails.sort((a, b) => {
+      const aLowestPrice =
+        a.prices.length > 0 ? a.prices[0].unit_amount : Infinity
+      const bLowestPrice =
+        b.prices.length > 0 ? b.prices[0].unit_amount : Infinity
+      return aLowestPrice - bLowestPrice
+    })
+
+    // Retornar los productos con sus precios asociados (ordenados de menor a mayor)
+    return sortedProductDetails
   } catch (error) {
     console.error('Error fetching products and prices from Stripe:', error)
     throw error
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
       ],
       mode: 'subscription', // O "payment" si es un pago único
       success_url: 'http://localhost:8000/success', // URL de redirección en caso de éxito
-      cancel_url: 'http://localhost:8000/suscription' // URL de redirección en caso de cancelación
+      cancel_url: 'http://localhost:3000/Suscription' // URL de redirección en caso de cancelación
     })
 
     res.json({ id: session.id })
