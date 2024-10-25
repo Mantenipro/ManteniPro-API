2e2ca2beb3b2d504b6806958f623e5fbb84eb26b
Luis Casas <luisscasas45@gmail.com>
2024-10-25 12:16:51 -0600

    FEAT/ endpoints for delete, put, get and getbyId

--- src/routes/equipment.router.js
+++ src/routes/equipment.router.js
@@ -3,16 +3,17 @@ const router = express.Router();
 const authMiddleware = require('../middleware/auth.middleware');
 const equipmentUseCase = require('../usecases/equipment.usecase'); 
 
+
 router.post('/', authMiddleware, async (req, res) => {
     try {
         if (!req.user || !req.user.id) {
             return res.status(401).json({ success: false, error: 'User not authenticated' });
         }
 
-        const { equipmentName, model, manufactureDate, brand, location, unitType, image, qr, owner } = req.body; // Agregado owner
+        const { equipmentName, model, manufactureDate, brand, location, unitType, image, qr, owner } = req.body; // Added owner
         const userId = req.user.id;
 
-        if (!equipmentName || !model || !manufactureDate || !brand || !location || !unitType || !owner) { // Agregado check para owner
+        if (!equipmentName || !model || !manufactureDate || !brand || !location || !unitType || !owner) { // Added check for owner
             return res.status(400).json({ success: false, error: 'Missing required fields' });
         }
 
@@ -41,7 +42,80 @@ router.post('/', authMiddleware, async (req, res) => {
     }
 });
 
+
+router.put('/:id', authMiddleware, async (req, res) => {
+    try {
+        const equipmentId = req.params.id;
+        const updatedData = req.body;
+
+        const updatedEquipment = await equipmentUseCase.editEquipment(equipmentId, updatedData);
+        
+        return res.status(200).json({ success: true, data: updatedEquipment });
+    } catch (error) {
+        console.error('Error editing equipment:', error.message);
+        console.error('Error stack trace:', error.stack);
+
+        if (error.status) {
+            return res.status(error.status).json({ success: false, error: error.message });
+        }
+        return res.status(500).json({ success: false, error: 'Error editing equipment' });
+    }
+});
+
+
+router.delete('/:id', authMiddleware, async (req, res) => {
+    try {
+        const equipmentId = req.params.id;
+
+        const deletedEquipment = await equipmentUseCase.deleteEquipment(equipmentId);
+        
+        return res.status(200).json({ success: true, data: deletedEquipment });
+    } catch (error) {
+        console.error('Error deleting equipment:', error.message);
+        console.error('Error stack trace:', error.stack);
+
+        if (error.status) {
+            return res.status(error.status).json({ success: false, error: error.message });
+        }
+        return res.status(500).json({ success: false, error: 'Error deleting equipment' });
+    }
+});
+
+
+router.get('/', authMiddleware, async (req, res) => {
+    try {
+        const equipmentList = await equipmentUseCase.getAllEquipment();
+        
+        return res.status(200).json({ success: true, data: equipmentList });
+    } catch (error) {
+        console.error('Error retrieving equipment list:', error.message);
+        console.error('Error stack trace:', error.stack);
+
+        return res.status(500).json({ success: false, error: 'Error retrieving equipment list' });
+    }
+});
+
+
+router.get('/:id', authMiddleware, async (req, res) => {
+    try {
+        const equipmentId = req.params.id;
+
+        const equipment = await equipmentUseCase.getEquipmentById(equipmentId);
+        
+        return res.status(200).json({ success: true, data: equipment });
+    } catch (error) {
+        console.error('Error retrieving equipment:', error.message);
+        console.error('Error stack trace:', error.stack);
+
+        if (error.status) {
+            return res.status(error.status).json({ success: false, error: error.message });
+        }
+        return res.status(500).json({ success: false, error: 'Error retrieving equipment' });
+    }
+});
+
 module.exports = router;
+
 
 
 
--- src/usecases/equipment.usecase.js
+++ src/usecases/equipment.usecase.js
@@ -1,6 +1,7 @@
 const Equipment = require('../models/equipment.model');
 const createError = require('http-errors');
 
+
 async function createEquipment(equipmentName, model, manufactureDate, brand, location, unitType, image, qr, userId, owner) {
   try {
     const equipmentFound = await Equipment.findOne({
@@ -40,9 +41,85 @@ async function createEquipment(equipmentName, model, manufactureDate, brand, loc
   }
 }
 
+
+async function editEquipment(id, updatedData) {
+  try {
+    const updatedEquipment = await Equipment.findByIdAndUpdate(id, updatedData, { new: true });
+
+    if (!updatedEquipment) {
+      throw createError(404, 'Equipment not found');
+    }
+
+    return updatedEquipment;
+  } catch (error) {
+    console.error('Error details:', error); 
+    if (error.status) {
+      throw error;
+    } else {
+      throw createError(500, 'Error updating equipment');
+    }
+  }
+}
+
+
+async function deleteEquipment(id) {
+  try {
+    const deletedEquipment = await Equipment.findByIdAndDelete(id);
+
+    if (!deletedEquipment) {
+      throw createError(404, 'Equipment not found');
+    }
+
+    return deletedEquipment;
+  } catch (error) {
+    console.error('Error details:', error); 
+    if (error.status) {
+      throw error;
+    } else {
+      throw createError(500, 'Error deleting equipment');
+    }
+  }
+}
+
+
+async function getAllEquipment() {
+  try {
+    const equipmentList = await Equipment.find();
+    return equipmentList;
+  } catch (error) {
+    console.error('Error details:', error); 
+    throw createError(500, 'Error retrieving equipment list');
+  }
+}
+
+
+async function getEquipmentById(id) {
+  try {
+    const equipment = await Equipment.findById(id);
+
+    if (!equipment) {
+      throw createError(404, 'Equipment not found');
+    }
+
+    return equipment;
+  } catch (error) {
+    console.error('Error details:', error); 
+    if (error.status) {
+      throw error;
+    } else {
+      throw createError(500, 'Error retrieving equipment');
+    }
+  }
+}
+
 module.exports = {
-  createEquipment
+  createEquipment,
+  editEquipment,
+  deleteEquipment,
+  getAllEquipment,
+  getEquipmentById
 };
+
 
 
 
