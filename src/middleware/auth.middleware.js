10b4b7382268e504983f020c91f52431e1afcbfa
Joseph Colin <joelbenjosephc@gmail.com>
2024-11-07 22:17:27 -0600

    Se mejora la seguridad de los inicio de sesion

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,7 +2,6 @@
 const createError = require('http-errors')
 const userUseCase = require('../usecases/users.usecase')
 const jwt = require('../lib/jwt')
-const Subscription = require('../models/subscription.model')
 
 async function auth(request, response, next) {
   try {
@@ -35,24 +34,4 @@ async function auth(request, response, next) {
   }
 }
 
-async function checkSubscription(req, res, next) {
-  const subscriptionId = req.user.subscriptionId
-
-  if (!subscriptionId) {
-    return res.status(403).send('No subscription found')
-  }
-
-  const subscription = await Subscription.findById(subscriptionId)
-
-  if (subscription.status !== true ) {
-    return res.status(403).send('Subscription is not active')
-  }
-
-  req.subscription = subscription
-  next()
-}
-
-module.exports = {
-  auth,
-  checkSubscription
-}
+module.exports = auth
